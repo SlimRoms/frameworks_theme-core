b52b20b2f6e891e365254a73ae28393c1b4469cf
Victor Lapin <victor@slimroms.org>
2017-02-20 23:45:44 +0300

    relocated Broadcast to core

---
+++ src/com/slimroms/themecore/Broadcast.java
@@ -0,0 +1,33 @@
+package com.slimroms.themecore;
+
+import android.content.IntentFilter;
+
+public class Broadcast {
+    public static final String ACTION_BACKEND_CONNECTED = "ACTION_BACKEND_CONNECTED";
+    public static final String ACTION_BACKEND_DISCONNECTED = "ACTION_BACKEND_DISCONNECTED";
+    public static final String ACTION_BACKEND_BUSY = "ACTION_BACKEND_BUSY";
+    public static final String ACTION_BACKEND_NOT_BUSY = "ACTION_BACKEND_NOT_BUSY";
+
+    public static final String EXTRA_BACKEND_NAME = "EXTRA_BACKEND_NAME";
+    public static final String EXTRA_THEME_PACKAGE = "EXTRA_THEME_PACKAGE";
+
+    private static IntentFilter mBackendConnectFilter;
+    public static IntentFilter getBackendConnectFilter() {
+        if (mBackendConnectFilter == null) {
+            mBackendConnectFilter = new IntentFilter();
+            mBackendConnectFilter.addAction(Broadcast.ACTION_BACKEND_CONNECTED);
+            mBackendConnectFilter.addAction(Broadcast.ACTION_BACKEND_DISCONNECTED);
+        }
+        return mBackendConnectFilter;
+    }
+
+    private static IntentFilter mBackendBusyFilter;
+    public static IntentFilter getBackendBusyFilter() {
+        if (mBackendBusyFilter == null) {
+            mBackendBusyFilter = new IntentFilter();
+            mBackendBusyFilter.addAction(ACTION_BACKEND_BUSY);
+            mBackendBusyFilter.addAction(ACTION_BACKEND_NOT_BUSY);
+        }
+        return mBackendBusyFilter;
+    }
+}
