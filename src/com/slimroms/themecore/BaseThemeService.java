358d59209ae2a2d3cfd5597f6d6400c49894cbf4
Josue Rivera <prbassplayer@gmail.com>
2017-06-14 00:48:27 +0200

    theme-core: Lower cache perms further
      * Since we are sharing a uid and no one needs to touch cache, use 700
    
    Change-Id: I47643465f5c17ad8f3a53353e101966a515e2ece
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- src/com/slimroms/themecore/BaseThemeService.java
+++ src/com/slimroms/themecore/BaseThemeService.java
@@ -126,7 +126,7 @@ public abstract class BaseThemeService extends Service {
             }
         }
         try {
-            Shell.chmod(cache.getAbsolutePath(), 744);
+            Shell.chmod(cache.getAbsolutePath(), 700);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -137,7 +137,7 @@ public abstract class BaseThemeService extends Service {
             }
         }
         try {
-            Shell.chmod(appCache.getAbsolutePath(), 744);
+            Shell.chmod(appCache.getAbsolutePath(), 700);
         } catch (Exception e) {
             e.printStackTrace();
         }
